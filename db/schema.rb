6e8d44deb83be54595dd543269c99b47d41533c3
Abdulhanan <abdulhanan347@gmail.com>
2022-08-08 11:56:41 +0500

    Added::Transctions: Added transctions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_055252) do
+ActiveRecord::Schema.define(version: 2022_08_08_062636) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "leftover_spent_in_cents"
+    t.integer "total_spent_in_cents"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
